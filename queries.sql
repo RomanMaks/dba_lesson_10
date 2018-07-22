eaafbc4ba10804dbb1208718ac1e124fdbab83dc
Maksimov Roman <maksimov.ro.al@gmail.com>
2018-07-22 15:42:46 +0300

    Добавлена таблица состав заказа

--- queries.sql
+++ queries.sql
@@ -7,11 +7,11 @@
       id SERIAL PRIMARY KEY,
       vendor_code CHAR(11) UNIQUE CHECK (length(vendor_code) = 11),
       name VARCHAR(100) CHECK (length(name) > 0),
-      cost NUMERIC(15, 2) CHECK (cost > 0),
-      picture VARCHAR(500),
-      admission_at DATE,
+      cost NUMERIC(15, 2) NOT NULL CHECK (cost > 0),
+      picture VARCHAR(500) NULL,
+      admission_at DATE NOT NULL,
       in_stock INTEGER CHECK (in_stock > 0),
-      description TEXT,
+      description TEXT NULL,
       properties jsonb NULL,
       cat_id INTEGER[] NULL,
       brand_id INTEGER NULL
@@ -20,33 +20,40 @@
   -- Категории
     CREATE TABLE categories (
       id SERIAL PRIMARY KEY,
-      name VARCHAR(100) CHECK (length(name) > 0)
+      name VARCHAR(100) NOT NULL CHECK (length(name) > 0)
     );
 
   -- Бренды
     CREATE TABLE brands (
       id SERIAL PRIMARY KEY,
-      name VARCHAR(100) CHECK (length(name) > 0)
+      name VARCHAR(100) NOT NULL CHECK (length(name) > 0)
     );
 
   -- Пользователи
     CREATE TABLE users (
       id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL CHECK (length(name) > 0),
-      surname VARCHAR(100) NULL,
-      patronymic VARCHAR(100) NULL,
-      phone VARCHAR(11) NOT NULL,
-      email VARCHAR(100) NULL
+      name VARCHAR(100) NOT NULL CHECK (length(name) > 0), -- Имя
+      surname VARCHAR(100) NULL, -- Фамилия
+      patronymic VARCHAR(100) NULL, -- Отчество
+      phone VARCHAR(11) NOT NULL, -- Телефон
+      email VARCHAR(100) NULL -- Email адрес
     );
 
   -- Заказы
     CREATE TABLE orders (
       id SERIAL PRIMARY KEY,
-      user_id INTEGER NOT NULL,
-      product_id INTEGER NOT NULL,
-      date_at DATE NOT NULL,
-      quantity INTEGER NOT NULL CHECK (quantity > 0),
-      total_cost NUMERIC(15, 2) CHECK (total_cost > 0)
+      user_id INTEGER NOT NULL, -- Пользователь
+      date_at DATE NOT NULL, -- Дата заказа
+      shipping_address TEXT NOT NULL, -- Адрес доставки
+      total_cost NUMERIC(15, 2) NOT NULL CHECK (total_cost > 0) -- Общая стоимость заказа
+    );
+
+  -- Состав заказа
+    CREATE TABLE order_items (
+      product_id INTEGER NOT NULL, -- Товар
+      order_id INTEGER NOT NULL, -- Заказ
+      quantity INTEGER NOT NULL CHECK (quantity > 0), -- Количество заказанного товара
+      PRIMARY KEY (product_id, order_id)
     );
 
 -- Связи:
@@ -55,4 +62,11 @@
     ADD FOREIGN KEY (brand_id)
     REFERENCES brands(id)
     ON UPDATE CASCADE
-    ON DELETE SET NULL;
+    ON DELETE SET NULL; 
+
+  -- Пользователи с заказами
+    ALTER TABLE orders
+    ADD FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT; -- Если у пользователя есть заказ то мы не сможем его удалить
