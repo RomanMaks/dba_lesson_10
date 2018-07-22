6594f7271376815fcb27436a606955eb34a7dff7
Maksimov Roman <maksimov.ro.al@gmail.com>
2018-07-22 13:24:38 +0300

    Добавлена таблица заказов, исправлены мелкие недочеты

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@
       id SERIAL PRIMARY KEY,
       vendor_code CHAR(11) UNIQUE CHECK (length(vendor_code) = 11),
       name VARCHAR(100) CHECK (length(name) > 0),
-      price INTEGER CHECK (price > 0),
+      cost NUMERIC(15, 2) CHECK (cost > 0),
       picture VARCHAR(500),
       admission_at DATE,
       in_stock INTEGER CHECK (in_stock > 0),
@@ -32,11 +32,21 @@
   -- Пользователи
     CREATE TABLE users (
       id SERIAL PRIMARY KEY,
-      name VARCHAR(100) CHECK (length(name) > 0) NOT NULL,
+      name VARCHAR(100) NOT NULL CHECK (length(name) > 0),
       surname VARCHAR(100) NULL,
       patronymic VARCHAR(100) NULL,
       phone VARCHAR(11) NOT NULL,
       email VARCHAR(100) NULL
+    );
+
+  -- Заказы
+    CREATE TABLE orders (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER NOT NULL,
+      product_id INTEGER NOT NULL,
+      date_at DATE NOT NULL,
+      quantity INTEGER NOT NULL CHECK (quantity > 0),
+      total_cost NUMERIC(15, 2) CHECK (total_cost > 0)
     );
 
 -- Связи:
